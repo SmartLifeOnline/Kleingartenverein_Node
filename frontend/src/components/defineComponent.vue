7464548aebec02c7a2b939d87946b6453fc44196
Michael Strübing <michael.struebing@belarion.de>
2024-11-03 23:16:21 +0100

    get started

---
+++ frontend/src/components/defineComponent.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+});
+</script>
+
+<template>
+  <div>{{ msg }}</div>
+</template>
